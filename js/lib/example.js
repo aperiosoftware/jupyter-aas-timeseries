3d5774932b2acfd3e1af7091b476e2dd15984cc7
Stuart <gravatar@strudel.org.uk>
2018-10-16 13:43:38 +0100

    Add TimeSeries javascript

--- js/lib/example.js
+++ js/lib/example.js
@@ -1,5 +1,16 @@
 var widgets = require('@jupyter-widgets/base');
+
 var _ = require('lodash');
+
+// Load the Javascript necessary for the TimeSeriess
+require('./aas-time-series-js/resources/stuquery.js');
+require('./aas-time-series-js/resources/graph.js');
+require('./aas-time-series-js/resources/timeseries.js');
+
+
+// We should have the resources at this point
+console.log('Loaded TimeSeries resources',S,Graph,TimeSeries)
+
 
 var TimeSeriesModel = widgets.DOMWidgetModel.extend({
     defaults: _.extend(widgets.DOMWidgetModel.prototype.defaults(), {
@@ -16,12 +27,22 @@ var TimeSeriesModel = widgets.DOMWidgetModel.extend({
 // Custom View. Renders the widget model.
 var TimeSeriesView = widgets.DOMWidgetView.extend({
     render: function() {
+    	console.log('render',this,this.model)
+
+    	// The problem here is that we can't render the widget *until*
+    	// we have the JSON that defines the size
+    	
+    	// Add a custom message callback
         this.model.on('msg:custom', this.handle_custom_message, this);
     },
 
     handle_custom_message: function(msg) {
-        this.el.textContent = JSON.stringify(msg);
-    }
+    	console.log('handle_custom_message',msg)
+    	// Make the instance of TimeSeries here
+    	// this.el references the DOM object
+    	var t = TimeSeries.create(msg,{});
+		t.initialize(this.el);  	
+s    }
 });
 
 
